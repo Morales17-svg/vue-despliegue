cc67a2017eaa5dfc211518c208561e1596c75613
Morales17 <avimorales58@gmail.com>
2024-11-10 18:04:52 -0300

    configuracion gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-
+  plugins: [vue(), ghPages()],
+  base: "vue-despliegue",
   resolve: {
     alias: {
       '@': '/src', // Configura el alias @ para apuntar a la carpeta src
